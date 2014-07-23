a864e10c3fd1c0c39d38bef0468d50376404a386
lamj1234 <jonathanlam123@gmail.com>
2014-07-22 17:28:03 -0700

    Update main.js
    server listen on Openshift

--- Server/main.js
+++ Server/main.js
@@ -223,6 +223,13 @@ completeResponse = function(dataObj, statusCode, contentType, content) {
 
 
 // Define and start the server.
-var server = http.createServer(serverCore);
-server.listen("80", "127.0.0.1");
-console.log("\nApp available at http://127.0.0.1:80\n");
+// var server = http.createServer(serverCore);
+// server.listen("80", "127.0.0.1");
+// console.log("\nApp available at http://127.0.0.1:80\n");
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
